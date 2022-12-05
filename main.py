f2f738e177efd66f03dd9f6a3b757744512a2999
pedro ramos <pedro.ramos9@hotmail.com>
2022-12-05 11:19:07 +0000

    added menu choice

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
+from secrets import choice
 from cifra.sorter import sorter
 
 from collections import OrderedDict
 
 cifraqwerty = "qwertyuiopasdfghjklzxcvbnm"
-initialword = "antonio"
-sampletext = "ola tudo bem"
+Choice = input("1-qwerty metode \n2-second metode \n")
 
-initialword = "".join(OrderedDict.fromkeys(initialword))
-print(initialword)
-cifra = sorter.create(initialword)
-print(cifra)
-encrypted_text = sorter.encrypttext(sampletext, cifra)
+if Choice == "1":
+    
+    sampletext=input("write your text\n")
+    qwertyencrypted = sorter.encrypttext(
+    sampletext, cifraqwerty)
+    print("texto encriptado usando o metodo qwerty:\n",
+    sorter.convertArrayToString(qwertyencrypted))
 
-print("texto encriptado: ", sorter.convertArrayToString(encrypted_text))
-print("texto desincreptado: ", sorter.convertArrayToString(
+    print("texto desincreptado usando o metodo qwerty:\n",
+    sorter.convertArrayToString(sorter.encrypttextreverse(qwertyencrypted, cifraqwerty)))
+
+elif Choice == "2":
+
+    initialword = input("write a word\n")
+    sampletext = input("write your text\n")
+
+    initialword = "".join(OrderedDict.fromkeys(initialword))
+    print(initialword)
+    cifra = sorter.create(initialword)
+    print(cifra)
+    encrypted_text = sorter.encrypttext(sampletext, cifra)
+
+    print("texto encriptado: \n", sorter.convertArrayToString(encrypted_text))
+    print("texto desincreptado: \n", sorter.convertArrayToString(
     sorter.encrypttextreverse(encrypted_text, cifra)))
 
-qwertyencrypted = sorter.encrypttext(
-    sampletext, cifraqwerty)
-print("texto encriptado usando o metodo qwerty: ",
-      sorter.convertArrayToString(qwertyencrypted))
 
-print("texto desincreptado usando o metodo qwerty: ",
-      sorter.convertArrayToString(sorter.encrypttextreverse(qwertyencrypted, cifraqwerty)))
